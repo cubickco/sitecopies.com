6171358261a5742412eec1f7b7bc61e852bbbbac
cubickco <work.pasternak@gmail.com>
2019-04-21 06:03:43 +0300

    first deploy

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://lp.loc/admin/');
+define('HTTP_CATALOG', 'http://lp.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://lp.loc/admin/');
+define('HTTPS_CATALOG', 'http://lp.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/docs/lp.loc/admin/');
+define('DIR_SYSTEM', '/opt/lampp/docs/lp.loc/system/');
+define('DIR_IMAGE', '/opt/lampp/docs/lp.loc/image/');
+define('DIR_LANGUAGE', '/opt/lampp/docs/lp.loc/admin/language/');
+define('DIR_TEMPLATE', '/opt/lampp/docs/lp.loc/admin/view/template/');
+define('DIR_CONFIG', '/opt/lampp/docs/lp.loc/system/config/');
+define('DIR_CACHE', '/opt/lampp/docs/lp.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/docs/lp.loc/system/storage/download/');
+define('DIR_LOGS', '/opt/lampp/docs/lp.loc/system/storage/logs/');
+define('DIR_MODIFICATION', '/opt/lampp/docs/lp.loc/system/storage/modification/');
+define('DIR_UPLOAD', '/opt/lampp/docs/lp.loc/system/storage/upload/');
+define('DIR_CATALOG', '/opt/lampp/docs/lp.loc/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'lp.loc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://lp.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://lp.loc/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/lampp/docs/lp.loc/catalog/');
+define('DIR_SYSTEM', '/opt/lampp/docs/lp.loc/system/');
+define('DIR_IMAGE', '/opt/lampp/docs/lp.loc/image/');
+define('DIR_LANGUAGE', '/opt/lampp/docs/lp.loc/catalog/language/');
+define('DIR_TEMPLATE', '/opt/lampp/docs/lp.loc/catalog/view/theme/');
+define('DIR_CONFIG', '/opt/lampp/docs/lp.loc/system/config/');
+define('DIR_CACHE', '/opt/lampp/docs/lp.loc/system/storage/cache/');
+define('DIR_DOWNLOAD', '/opt/lampp/docs/lp.loc/system/storage/download/');
+define('DIR_LOGS', '/opt/lampp/docs/lp.loc/system/storage/logs/');
+define('DIR_MODIFICATION', '/opt/lampp/docs/lp.loc/system/storage/modification/');
+define('DIR_UPLOAD', '/opt/lampp/docs/lp.loc/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'lp.loc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
